40b7d3b3f279d3bc42ab1118a7601e6dd4c8ad7a
Devin <991476256@qq.com>
2020-07-27 16:05:00 +0800

    [devin] refactor: extract constant CALCULATE_ERROR

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -9,6 +9,7 @@ public class WordFrequencyGame {
     private static final String SPACE_PATTERN = "\\s+";
     private static final String NEW_LINE_DELIMITER = "\n";
     private static final String BLANK_SPACE = " ";
+    private static final String CALCULATE_ERROR = "Calculate Error";
 
     public String getResult(String sentence) {
 
@@ -44,7 +45,7 @@ public class WordFrequencyGame {
                 }
                 return joiner.toString();
             } catch (Exception e) {
-                return "Calculate Error";
+                return CALCULATE_ERROR;
             }
         }
     }
